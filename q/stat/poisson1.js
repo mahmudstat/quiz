e077168839645c1314336c6598bbd2630f7b5408
mahmudstat <almahmud.sbi@gmail.com>
2024-10-28 23:40:34 +0600

    add poisson 1

---
+++ q/stat/poisson1.js
@@ -0,0 +1,37 @@
+const quizData = [
+    {
+        question: "The Poisson distribution is typically used to model which type of events?",
+        options: ["Events that are equally spaced", "Rare, random events over a fixed interval", "Periodic events", "Continuous events"],
+        correct: "Rare, random events over a fixed interval"
+    },
+    {
+        question: "In a Poisson distribution, what does the parameter λ (lambda) represent?",
+        options: ["Variance of events", "Average number of occurrences in a given interval", "Probability of success", "Fixed number of trials"],
+        correct: "Average number of occurrences in a given interval"
+    },
+    {
+        question: "What is the mean (expected value) of a Poisson distribution with parameter λ?",
+        options: ["1/λ", "λ^2", "λ", "2λ"],
+        correct: "λ"
+    },
+    {
+        question: "What is the variance of a Poisson distribution with parameter λ?",
+        options: ["λ", "√λ", "λ^2", "2λ"],
+        correct: "λ"
+    },
+    {
+        question: "For which type of data is the Poisson distribution most appropriate?",
+        options: ["Continuous data", "Count data", "Ordinal data", "Binary data"],
+        correct: "Count data"
+    },
+    {
+        question: "Which of the following is a key assumption of the Poisson distribution?",
+        options: ["Events occur in clusters", "Events are evenly spaced", "Events are independent", "Events are periodic"],
+        correct: "Events are independent"
+    },
+    {
+        question: "What happens to the shape of the Poisson distribution as λ increases?",
+        options: ["It becomes more skewed to the right", "It approaches a normal distribution", "It becomes more skewed to the left", "It becomes constant"],
+        correct: "It approaches a normal distribution"
+    }
+];
