61256ce28af080c46bb1d697163f22fed2be7a72
mahmudstat <almahmud.sbi@gmail.com>
2024-10-28 23:40:53 +0600

    rmv mathematical ones

--- q/stat/binom1.js
+++ q/stat/binom1.js
@@ -1,8 +1,8 @@
 const quizData = [
             {
-                question: "What is the probability of getting exactly 3 heads in 5 flips of a fair coin?",
-                options: ["0.5", "0.3125", "0.25", "0.2"],
-                correct: "0.3125"
+        question: "Which of the following conditions must be met for a distribution to be considered binomial?",
+        options: ["Only one trial", "A fixed number of trials", "Variable probability of success", "Unlimited trials"],
+        correct: "A fixed number of trials"
             },
             {
                 question: "In a binomial distribution, what does 'n' represent?",
@@ -15,13 +15,18 @@ const quizData = [
                 correct: "Binomial distribution"
             },
             {
-                question: "If a fair die is rolled 6 times, what is the probability of getting exactly 2 sixes?",
-                options: ["0.5", "0.2613", "0.5", "0.38"],
-                correct: "0.2613"
-            },
-            {
                 question: "In a binomial distribution, what does 'p' represent?",
                 options: ["Number of trials", "Probability of success", "Number of successes", "None of the above"],
                 correct: "Probability of success"
-            }
+            },
+                {
+        question: "What is the mean (expected value) of a binomial distribution with parameters 'n' and 'p'?",
+        options: ["n / p", "n * p", "p / n", "n + p"],
+        correct: "n * p"
+    },
+        {
+        question: "Which formula represents the variance of a binomial distribution with parameters 'n' and 'p'?",
+        options: ["np(1 - p)", "np + p", "n^2 * p", "p / n"],
+        correct: "np(1 - p)"
+    }
         ];
